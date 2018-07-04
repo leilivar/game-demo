bae37e98f9012a6b2bdb47d3ad6a3b92f78f8502
leili <leili@cocountislandstudio.com>
2018-07-05 00:15:10 +0800

    assets commit

---
+++ Assets/Scenes/DebugScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugScript : MonoBehaviour {
+    public Slider slider;
+    public Text sliderText;
+    public Text input;
+    public Text errorText;
+    private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        slider.value = 1;
+        OnSliderChanged();
+        //GetComponent<InputField>().text = "";
+        EventManager.Instance.Register("DebugBeat", PlayDebugBeat);
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();
+        }
+	}
+
+    public void OnSliderChanged(){
+        sliderText.text = "每拍的时间:" + slider.value;
+    }
+
+    public void PlayBeats(){
+        try
+        {
+            SongInfo song = SongInfo.LoadDebugBeat(slider.value, input.text);
+            MusicManager.Instance.StartSong(song);
+        }catch(System.Exception e){
+            errorText.text = "输入的节拍好像有点问题:)";
+            CoroutineManager.Instance.DelayedAction(2, () =>
+            {
+                errorText.text = "";
+            });
+        }
+    }
+
+    public void PlayDebugBeat(GameObject sender, object arg){
+        Debug.Log("beep");
+        audio.Play();
+    }
+}
